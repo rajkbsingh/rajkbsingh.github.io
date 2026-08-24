da0b8e5adfe06b3f8edec9bb6f8949aa18bf980a
Raj Ku Baliyar Singh <154069125+rajkbsingh@users.noreply.github.com>
2026-08-24 21:52:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-// Current year
 document.getElementById("year").textContent =
     new Date().getFullYear();
 
 
-// Dark / Light mode
-const themeToggle = document.getElementById("themeToggle");
+const themeToggle =
+    document.getElementById("themeToggle");
+
 
 themeToggle.addEventListener("click", () => {
 
@@ -20,10 +20,10 @@ themeToggle.addEventListener("click", () => {
         "theme",
         darkMode ? "dark" : "light"
     );
+
 });
 
 
-// Restore saved theme
 const savedTheme =
     localStorage.getItem("theme");
 
@@ -32,15 +32,16 @@ if (savedTheme === "dark") {
     document.body.classList.add("dark-mode");
 
     themeToggle.textContent = "☀️";
+
 }
 
 
-// Active navigation link
 const sections =
     document.querySelectorAll("section[id]");
 
 const navLinks =
     document.querySelectorAll("nav a");
+
 
 window.addEventListener("scroll", () => {
 
@@ -52,9 +53,14 @@ window.addEventListener("scroll", () => {
             section.offsetTop - 150;
 
         if (window.scrollY >= sectionTop) {
-            current = section.getAttribute("id");
+
+            current =
+                section.getAttribute("id");
+
         }
+
     });
+
 
     navLinks.forEach(link => {
 
@@ -64,30 +70,8 @@ window.addEventListener("scroll", () => {
             link.getAttribute("href") ===
             `#${current}`
         ) {
+
             link.classList.add("active");
-        }
-    });
-
-});
-
-
-// Smooth navigation
-navLinks.forEach(link => {
-
-    link.addEventListener("click", function(event) {
-
-        event.preventDefault();
-
-        const target =
-            document.querySelector(
-                this.getAttribute("href")
-            );
-
-        if (target) {
-
-            target.scrollIntoView({
-                behavior: "smooth"
-            });
 
         }
 
